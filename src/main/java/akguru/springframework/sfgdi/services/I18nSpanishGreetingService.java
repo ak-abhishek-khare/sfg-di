4c0b4f6d24366198e2a4f53523a4790129295aed
Abhishek Khare <abhishek.khare@dxc.com>
2020-11-30 23:16:30 +0530

    Fourth Commit for Spring default Profile

--- src/main/java/akguru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/akguru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package akguru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("SP")
+@Profile({"SP","default"})
 @Service("I18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
